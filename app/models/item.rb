9104d7dafa838f71fa78ce8119a43449ab05da56
marie1173 <marie.ohto@gmail.com>
2022-12-19 20:00:48 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
+  def was_attached?
+    self.image.attached?
+  end
   validates :item_name, presence: true
   validates :item_text, presence: true
   validates :category_id, presence: true
@@ -17,7 +21,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :day_id, presence: true
   validates :price, format: { with: /\A[0-9]+\z/ }
-  validates :price, numericality: { in: 300..9_999_999 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :price, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
