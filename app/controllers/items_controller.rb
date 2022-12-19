e204db95aaa2fb0ebdcf117ddc1c1cd2b60ea1aa
marie1173 <marie.ohto@gmail.com>
2022-12-19 13:21:11 +0900

    newHTMLファイルとコントローラを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name,:item_text,:category_id,:condition_id,:fee_id,:prefecture_id,:day_id,:price).merge(user_id: current_user.id)
+  end
+
 end
