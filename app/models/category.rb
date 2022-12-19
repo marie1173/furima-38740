c944fa1dcba6177d6d67b794cba34adf4e4d551b
marie1173 <marie.ohto@gmail.com>
2022-12-19 17:07:08 +0900

    出品画面の作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: 'レディース' },
--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Condition < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '新品・未使用' },
@@ -6,7 +6,7 @@ class Genre < ActiveHash::Base
     { id: 4, name: '目立った傷や汚れなし' },
     { id: 5, name: 'やや傷や汚れあり' },
     { id: 6, name: '傷や汚れあり' },
-    { id: 7, name: '全体的に状態が悪い' },
+    { id: 7, name: '全体的に状態が悪い' }
   ]
 
   include ActiveHash::Associations
