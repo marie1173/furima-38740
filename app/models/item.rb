cb2cea6d2cd5fe2e4baa389e67113f775b4e4347
marie1173 <marie.ohto@gmail.com>
2022-12-19 11:30:17 +0900

    ActiveHashとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :day
+
   belongs_to :user
   has_one_attached :image
+
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :day_id, presence: true
+  validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
