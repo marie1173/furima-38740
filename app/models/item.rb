f4882d3b663f8f4c7eb43f0be67536167b462c39
marie1173 <marie.ohto@gmail.com>
2022-12-26 11:42:48 +0900

    指摘箇所修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,10 +21,8 @@ class Item < ApplicationRecord
   validates :fee_id, presence: true
   validates :prefecture_id, presence: true
   validates :scheduled_day_id, presence: true
-  validates :price, numericality: {only_integer: true}
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :price, presence: true
-
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
